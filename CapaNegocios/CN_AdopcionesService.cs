68e61bc62841d2c1a60f106da334a502fcc332f3
dayanas08 <dayamormio2016@gmail.com>
2025-12-27 21:45:37 -0500

    feat: acciones de adopciones (solicitar, aprobar, rechazar)

---
+++ CapaNegocios/CN_AdopcionesService.cs
@@ -0,0 +1,54 @@
+﻿using CapaDatos;
+using CapaDatos.CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocios
+{
+    public class CN_AdopcionesService
+    {
+            private CD_Adopciones datos = new CD_Adopciones();
+
+            // Acción: SolicitarAdopcion
+            public void SolicitarAdopcion(int idMascota, int idUsuario, string comentario)
+            {
+                tbl_SolicitudesAdopcion solicitud = new tbl_SolicitudesAdopcion
+                {
+                    sol_IdMascota = idMascota,
+                    sol_IdUsuario = idUsuario,
+                    sol_Comentarios = comentario,
+                    sol_Estado = "Pendiente",
+                    sol_FechaSolicitud = DateTime.Now
+                };
+
+                datos.SolicitarAdopcion(solicitud);
+            }
+
+            // Acción: MisSolicitudes (usuario)
+            public List<tbl_SolicitudesAdopcion> MisSolicitudes(int idUsuario)
+            {
+                return datos.ObtenerSolicitudesPorUsuario(idUsuario);
+            }
+
+            // Acción: SolicitudesRecibidas (refugio)
+            public List<tbl_SolicitudesAdopcion> SolicitudesRecibidas(int idRefugio)
+            {
+                return datos.ObtenerSolicitudesPorRefugio(idRefugio);
+            }
+
+            // Acción: Aprobar
+            public void Aprobar(int idSolicitud, int idUsuarioRevision)
+            {
+                datos.AprobarSolicitud(idSolicitud, idUsuarioRevision);
+            }
+
+            // Acción: Rechazar
+            public void Rechazar(int idSolicitud, string comentario, int idUsuarioRevision)
+            {
+                datos.RechazarSolicitud(idSolicitud, comentario, idUsuarioRevision);
+            }
+        }
+}
